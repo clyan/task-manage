494f409422040f72e9e83106ac534f31f29b9857
ywymoshi <46644748+ywymoshi@users.noreply.github.com>
2021-05-03 01:48:15 +0800

    refactor: :recycle: use-undo中，使用useReduecer改进，抽取逻辑，合并状态

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,25 +1,16 @@
-import { useCallback } from "react";
+import { useCallback, useReducer } from "react";
 import { useState } from "react";
 
-// 状态合并
-export const useUndo = <T>(initialPresent: T) => {
-  const [state, setState] = useState<{
-    past: T[];
-    present: T;
-    future: T[];
-  }>({
-    past: [],
-    present: initialPresent,
-    future: [],
-  });
-  const canUndo = state.past.length !== 0;
-  const canRedo = state.future.length !== 0;
-  // 使用useCallback包裹函数的依赖时，所有函数依赖都要加上，很麻烦
-  const undo = useCallback(() => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
-
-      if (past.length === 0) return currentState;
+const UNDO = "UNDO";
+const REDO = "REDO";
+const SET = "SET";
+const RESET = "RESET";
+const undoReducer = <T>(state: State<T>, action: Action<T>) => {
+  const { past, present, future } = state;
+  const { type, newPresent } = action;
+  switch (type) {
+    case UNDO: {
+      if (past.length === 0) return state;
 
       const previous = past[past.length - 1];
       const newPast = past.slice(0, past.length - 1);
@@ -28,14 +19,9 @@ export const useUndo = <T>(initialPresent: T) => {
         present: previous,
         future: [present, ...future],
       };
-    });
-  }, []);
-  const redo = useCallback(() => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
-
-      if (future.length === 0) return currentState;
-
+    }
+    case REDO: {
+      if (future.length === 0) return state;
       const next = future[0];
       const newFuture = past.slice(1);
       return {
@@ -43,29 +29,67 @@ export const useUndo = <T>(initialPresent: T) => {
         present: next,
         future: newFuture,
       };
-    });
-  }, []);
-  const set = useCallback((newPresent: T) => {
-    setState((currentState) => {
-      const { past, present } = currentState;
+    }
+    case SET: {
       if (newPresent === present) {
-        return currentState;
+        return state;
       }
       return {
         past: [...past, present],
         present: newPresent,
         future: [],
       };
-    });
-  }, []);
-
-  const reset = useCallback((newPresent: T) => {
-    setState(() => {
+    }
+    case RESET: {
       return {
         past: [],
         present: newPresent,
         future: [],
       };
+    }
+  }
+};
+type State<T> = {
+  past: T[];
+  present: T;
+  future: T[];
+};
+type Action<T> = {
+  newPresent?: T;
+  type: typeof UNDO | typeof REDO | typeof SET | typeof RESET;
+};
+// 状态合并
+export const useUndo = <T>(initialPresent: T) => {
+  const [state, dispatch] = useReducer(undoReducer, {
+    past: [],
+    present: initialPresent,
+    future: [],
+  } as State<T>);
+
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+  // 使用useCallback包裹函数的依赖时，所有函数依赖都要加上，很麻烦
+  const undo = useCallback(() => {
+    dispatch({
+      type: UNDO,
+    });
+  }, []);
+  const redo = useCallback(() => {
+    dispatch({
+      type: REDO,
+    });
+  }, []);
+  const set = useCallback((newPresent: T) => {
+    dispatch({
+      type: SET,
+      newPresent,
+    });
+  }, []);
+
+  const reset = useCallback((newPresent: T) => {
+    dispatch({
+      type: RESET,
+      newPresent,
     });
   }, []);
   return [state, { set, reset, undo, redo, canUndo, canRedo }] as const;
